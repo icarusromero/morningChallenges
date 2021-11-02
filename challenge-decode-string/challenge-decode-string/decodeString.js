8b63d7fe437c0a030f9b097c58391cbb29855fe3
Icarus Romero <cloudkingdom07@yahoo.com>
2021-11-02 10:01:06 -0500

    wrong number was being subtracted from position and num was not an integer

--- challenge-decode-string/challenge-decode-string/decodeString.js
+++ challenge-decode-string/challenge-decode-string/decodeString.js
@@ -3,7 +3,8 @@
 function caesarCipher(str) {
     str = str.split('')
 
-    const num = str.shift();
+    let num = str.shift();
+    num = Number(num);
 
     let alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -13,16 +14,15 @@ function caesarCipher(str) {
     for(let i = 0; i < str.length; i++){
         for(let x = 0; x < alph.length; x++){
             if(str[i] === alph[x]){
-                let newPos = x + num;
-                console.log(x)
-                if(newPos > 23){
-                    newPos = newPos - 23;
+                let newPos = (x + num);
+                if(newPos > 25){
+                    newPos = (newPos - 26);
                 }
                 decode.push(alph[newPos]);
             }
         }
     }
-    return decode
+    return decode.join("")
 }
 
-console.log(caesarCipher('2fcjjm'))
+console.log(caesarCipher('6gcmnubmnyyf'))
